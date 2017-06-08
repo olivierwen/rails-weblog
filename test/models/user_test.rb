6f8815776ea510791c89cde55571ece03c8131cc
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2017-06-08 21:48:31 +0200

    Exercise 1
    Solution of
    https://gist.github.com/thibaudgg/68d6e182d986c1c75f109dd1b2a0d6df#exerc
    ise-1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#display_name when admin' do
+    user = User.new(admin: true, name: 'Bob')
+
+    assert_equal 'Bob (admin)', user.display_name
+  end
+
+  test '#display_name when not admin' do
+    user = User.new(admin: false, name: 'Bob')
+
+    assert_equal 'Bob', user.display_name
+  end
 end
