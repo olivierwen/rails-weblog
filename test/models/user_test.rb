ac98046d9535b06dfa8fe6fc3191e5738a33cf5e
Thibaud <thibaud@thibaud.gg>
2017-06-13 08:51:19 +0000

    Exercise 2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,10 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(admin:false,name:'Bob')
     assert_equal 'Bob', user.display_name
   end
+
+  test 'ensures that password length is min 8' do
+    user = User.new(password: '1234567')
+    assert_not user.valid?
+    assert_equal [:password], user.errors.keys
+ end
 end
